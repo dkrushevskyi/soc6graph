c9734851368490bdafc6e06ff0accec8309d5d66
dkrushevskyi <d.krushevskyi@gmail.com>
2021-08-24 23:11:54 +0300

    Refactor facebook settings dto

--- src/main/java/com/soc6graph/socgraph/config/dto/FacebookSettings.java
+++ src/main/java/com/soc6graph/socgraph/config/dto/FacebookSettings.java
@@ -1,14 +1,12 @@
 package com.soc6graph.socgraph.config.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "spring.social.facebook")
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Setter
 public class FacebookSettings {
 
     private String appId;
