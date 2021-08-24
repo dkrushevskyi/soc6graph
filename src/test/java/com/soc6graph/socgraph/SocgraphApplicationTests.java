90464cb126719a9b85df54540eb50b145a0230fa
dkrushevskyi <d.krushevskyi@gmail.com>
2021-08-24 22:55:56 +0300

    First init commit

---
+++ src/test/java/com/soc6graph/socgraph/SocgraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soc6graph.socgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocgraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
